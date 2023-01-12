0bcdf0a534a3dd17c03fcb84ea6026414354a1fb
delhomvi <117632201+delhomvi@users.noreply.github.com>
2023-01-12 17:13:26 +0100

    add all component

--- Angular/WebApp-FilRouge/src/app/app.module.ts
+++ Angular/WebApp-FilRouge/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CorpusComponent } from './corpus/corpus.component';
+import { CardsComponent } from './corpus/cards/cards.component';
+import { SearchbarComponent } from './header/searchbar/searchbar.component';
+import { HamburgerMenuComponent } from './header/hamburger-menu/hamburger-menu.component';
+import { ButtonsComponent } from './header/buttons/buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    CorpusComponent,
+    CardsComponent,
+    SearchbarComponent,
+    HamburgerMenuComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule
