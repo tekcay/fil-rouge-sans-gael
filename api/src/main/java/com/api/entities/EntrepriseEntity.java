8221f1650153c98ce0499037b59664cb88d05736
taaaaa111 <taos.tou@gmail.com>
2023-01-13 12:13:51 +0100

    added Entreprise entity with getter/setter & empty constructor

---
+++ api/src/main/java/com/api/entities/EntrepriseEntity.java
@@ -0,0 +1,13 @@
+package com.api.entities;
+
+import lombok.Data;
+
+@Data
+public class EntrepriseEntity {
+    int id;
+    String mail;
+    String name;
+
+    public EntrepriseEntity() {
+    }
+}
