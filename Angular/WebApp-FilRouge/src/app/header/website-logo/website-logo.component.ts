8018516b407f1008ed33a05efc7618ae378a7a51
delhomvi <117632201+delhomvi@users.noreply.github.com>
2023-01-13 16:58:41 +0100

    modified main page presentation, add bg, modify border radius and footer

---
+++ Angular/WebApp-FilRouge/src/app/header/website-logo/website-logo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-website-logo',
+  templateUrl: './website-logo.component.html',
+  styleUrls: ['./website-logo.component.scss']
+})
+export class WebsiteLogoComponent {
+
+}
