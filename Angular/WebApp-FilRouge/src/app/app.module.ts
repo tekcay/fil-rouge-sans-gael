f0c8534dbaa68f48c1991886a833d195aed36987
delhomvi <117632201+delhomvi@users.noreply.github.com>
2023-01-13 09:36:44 +0100

    initial commit

--- Angular/WebApp-FilRouge/src/app/app.module.ts
+++ Angular/WebApp-FilRouge/src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { CorpusComponent } from './corpus/corpus.component';
 import { CardsComponent } from './corpus/cards/cards.component';
 import { SearchbarComponent } from './header/searchbar/searchbar.component';
 import { HamburgerMenuComponent } from './header/hamburger-menu/hamburger-menu.component';
 import { ButtonsComponent } from './header/buttons/buttons.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
