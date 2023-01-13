d78574df02698ecafed32404c15b73aa44095809
tekcay <pierreblc@hotmail.fr>
2023-01-13 14:44:08 +0100

    added relationship between Entreprise and Stagiaire classes

--- api/src/main/java/com/api/entities/Entreprise.java
+++ api/src/main/java/com/api/entities/Entreprise.java
@@ -2,11 +2,23 @@ package com.api.entities;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
 @Data
+@Entity
 public class Entreprise {
+
+    @Id
+    @GeneratedValue
     private int id;
     private String mail;
     private String name;
+    @OneToMany(mappedBy = "entreprise")
+    private List<Stagiaire> stagiaireList;
 
     public Entreprise() {
     }
--- api/src/main/java/com/api/entities/Stagiaire.java
+++ api/src/main/java/com/api/entities/Stagiaire.java
@@ -3,17 +3,30 @@ package com.api.entities;
 
 
 import ch.qos.logback.core.joran.sanity.Pair;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
 @Data
+@Entity
 public class Stagiaire {
+
+    @Id
+    @GeneratedValue
     private int id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String mail;
     private List<Pair<Formation,Integer>> formationsNote;
     private boolean isPro;
+
+    @ManyToOne
+    @JoinColumn(name = "id_entreprise")
     private Entreprise Entreprise;
+
 
     public Stagiaire() {
     }
