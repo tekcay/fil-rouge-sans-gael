2d1ffc4684da30b24159e5f00520830915287d78
tekcay <pierreblc@hotmail.fr>
2023-01-13 16:35:15 +0100

    started work on tests

--- api/src/test/java/com/api/ApiApplicationTests.java
+++ api/src/test/java/com/api/ApiApplicationTests.java
@@ -3,12 +3,23 @@ package com.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 @SpringBootTest
-class ApiApplicationTests {
+public class ApiApplicationTests {
+
 
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("fil-rouge");
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction transaction = em.getTransaction();
+
 	}
 
 }
