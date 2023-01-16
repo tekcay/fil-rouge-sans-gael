e28da0d104e5db7c6a5e937610419c8ea12b470d
delhomvi <117632201+delhomvi@users.noreply.github.com>
2023-01-16 10:50:16 +0100

    add home to hamburger mun and rooterLink it to home component TODO: - link tiles to their page

--- Angular/WebApp-FilRouge/src/app/app-routing.module.ts
+++ Angular/WebApp-FilRouge/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 import { CardsComponent } from './corpus/cards/cards.component';
 import { CreateFormaComponent } from './corpus/create-forma/create-forma.component';
 import { ListFormaComponent } from './corpus/list-forma/list-forma.component';
@@ -9,11 +10,19 @@ import { UserProfileComponent } from './corpus/user-profile/user-profile.compone
 
 
 const routes: Routes = [
-  { path: 'cards', component: CardsComponent },
-  { path: 'create', component: CreateFormaComponent },
-  { path: 'liste', component: ListFormaComponent },
-  { path: 'search', component: SearchFormaComponent },
-  { path: 'profile', component: UserProfileComponent }
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'cards',
+    component: CardsComponent,
+    children:[
+      { path: 'create', component: CreateFormaComponent },
+      { path: 'liste', component: ListFormaComponent },
+      { path: 'search', component: SearchFormaComponent },
+      { path: 'profile', component: UserProfileComponent }
+    ]
+  }
+
+
 ];
 
 @NgModule({
