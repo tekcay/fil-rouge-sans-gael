96201a2338f3c6d6bd1710a0e81b8c304bad4e2f
delhomvi <117632201+delhomvi@users.noreply.github.com>
2023-01-16 13:53:04 +0100

    add links at the bottom

---
+++ Angular/WebApp-FilRouge/src/app/policies/policies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-policies',
+  templateUrl: './policies.component.html',
+  styleUrls: ['./policies.component.scss']
+})
+export class PoliciesComponent {
+
+}
---
+++ Angular/WebApp-FilRouge/src/app/propsection/propsection.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-propsection',
+  templateUrl: './propsection.component.html',
+  styleUrls: ['./propsection.component.scss']
+})
+export class PropsectionComponent {
+
+}
