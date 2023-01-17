9a02c5e91501c87be36e7ca80929ef929dcdae37
delhomvi <117632201+delhomvi@users.noreply.github.com>
2023-01-17 13:50:10 +0100

    7: (need fix) update forms with ngForm, nothing apear

--- Angular/WebApp-FilRouge/src/app/corpus/create-forma/create-forma.component.ts
+++ Angular/WebApp-FilRouge/src/app/corpus/create-forma/create-forma.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { IFormation } from 'src/app/interfaces/formation';
 
 @Component({
   selector: 'app-create-forma',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-forma.component.scss']
 })
 export class CreateFormaComponent {
+    formation: FormGroup = this.fb.group({
+      id:'0',
+      theme:undefined!,
+      stheme:undefined!,
+      sstheme:undefined!,
+      seances:undefined!,
+      formateurs:undefined!,
+      isConfirmed:false,
+      isPerso:false,
+      isIntra:false,
+      minPart:0,
+      nbPart:0,
+      link:'',
+      price:0,
+    })
 
+    constructor(private fb: FormBuilder){}
+
+    onSubmitFormation(){
+      console.log("SendForm")
+    }
 }
