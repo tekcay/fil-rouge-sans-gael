37a610fa42b6dd62820fb34de6e8b2c3a0668cee
delhomvi <117632201+delhomvi@users.noreply.github.com>
2023-01-17 15:16:05 +0100

    9: update all forms, bring back past version

--- Angular/WebApp-FilRouge/src/app/corpus/create-forma/create-forma.component.ts
+++ Angular/WebApp-FilRouge/src/app/corpus/create-forma/create-forma.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
-import { IFormation } from 'src/app/interfaces/formation';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-create-forma',
@@ -8,25 +7,5 @@ import { IFormation } from 'src/app/interfaces/formation';
   styleUrls: ['./create-forma.component.scss']
 })
 export class CreateFormaComponent {
-    formation: FormGroup = this.fb.group({
-      id:'0',
-      theme:undefined!,
-      stheme:undefined!,
-      sstheme:undefined!,
-      seances:undefined!,
-      formateurs:undefined!,
-      isConfirmed:false,
-      isPerso:false,
-      isIntra:false,
-      minPart:0,
-      nbPart:0,
-      link:'',
-      price:0,
-    })
 
-    constructor(private fb: FormBuilder){}
-
-    onSubmitFormation(){
-      console.log("SendForm")
-    }
 }
