2737b7b7b64bc2feeb1d6848758053eaef89c18d
delhomvi <117632201+delhomvi@users.noreply.github.com>
2023-01-17 15:47:30 +0100

    11: add list of formation

--- Angular/WebApp-FilRouge/src/app/corpus/cards/cards-list/cards-list.component.ts
+++ Angular/WebApp-FilRouge/src/app/corpus/cards/cards-list/cards-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IFormation } from 'src/app/interfaces/formation';
 
 @Component({
   selector: 'app-cards-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./cards-list.component.scss']
 })
 export class CardsListComponent {
-
+  formations : IFormation[]=[
+    {id:'0',theme:undefined!,stheme:undefined!,sstheme:undefined!,seances:undefined!,formateurs:undefined!,isConfirmed:true,isIntra:true,isPerso:false,minPart:10,nbPart:5,link:"toto.fr",price:500},
+    {id:'1',theme:undefined!,stheme:undefined!,sstheme:undefined!,seances:undefined!,formateurs:undefined!,isConfirmed:true,isIntra:true,isPerso:false,minPart:10,nbPart:6,link:"tata.fr",price:400}
+  ]
 }
--- Angular/WebApp-FilRouge/src/app/corpus/list-forma/list-forma.component.ts
+++ Angular/WebApp-FilRouge/src/app/corpus/list-forma/list-forma.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IFormation } from 'src/app/interfaces/formation';
 
 @Component({
   selector: 'app-list-forma',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-forma.component.scss']
 })
 export class ListFormaComponent {
+  formations : IFormation[]=[
+    {id:'0',theme:undefined!,stheme:undefined!,sstheme:undefined!,seances:undefined!,formateurs:undefined!,isConfirmed:true,isIntra:true,isPerso:false,minPart:10,nbPart:5,link:"toto.fr",price:500},
+    {id:'1',theme:undefined!,stheme:undefined!,sstheme:undefined!,seances:undefined!,formateurs:undefined!,isConfirmed:true,isIntra:true,isPerso:false,minPart:10,nbPart:6,link:"tata.fr",price:400}
+  ]
 
 }
