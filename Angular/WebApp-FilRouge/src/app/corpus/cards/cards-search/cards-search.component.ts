0bf1509e1f2c1fe53ae4c426a366d2f5a1239e8e
delhomvi <117632201+delhomvi@users.noreply.github.com>
2023-01-18 09:55:39 +0100

    20: update search form

--- Angular/WebApp-FilRouge/src/app/corpus/cards/cards-search/cards-search.component.ts
+++ Angular/WebApp-FilRouge/src/app/corpus/cards/cards-search/cards-search.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ISTheme } from 'src/app/interfaces/s-theme';
+import { ISsTheme } from 'src/app/interfaces/ss-theme';
+import { ITheme } from 'src/app/interfaces/theme';
 
 @Component({
   selector: 'app-cards-search',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./cards-search.component.scss']
 })
 export class CardsSearchComponent {
+  themes: ITheme[] = [
+    {id:'0',name:'Developpement web',SThemeList:[]},
+    {id:'1',name:'Developpement jeu mobile',SThemeList:[]},
+    {id:'2',name:'Developpement jeu video',SThemeList:[]},
+    {id:'3',name:'Developpement applicatioon',SThemeList:[]}
+  ]
 
+  sthemes: ISTheme[] = [
+    {id:'0',name:'Angular',SSThemeList:[]},
+    {id:'1',name:'Java',SSThemeList:[]},
+  ]
+
+  ssthemes: ISsTheme[] = [
+    {id:'0',name:'CRUD'},
+    {id:'1',name:'Interface'}
+  ]
 }
