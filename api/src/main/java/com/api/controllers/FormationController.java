21ec05c390940f60818cfdd92ddc7c89d509c9ee
tekcay <pierreblc@hotmail.fr>
2023-01-18 11:11:29 +0100

    added basic queries

---
+++ api/src/main/java/com/api/controllers/FormationController.java
@@ -0,0 +1,36 @@
+package com.api.controllers;
+
+import com.api.entities.Formation;
+import com.api.repositories.FormationRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/formation-controller")
+public class FormationController {
+
+    @Autowired
+    private FormationRepo formationRepo;
+
+    @GetMapping("/formations")
+    public List<Formation> getAllFormation() {
+        return formationRepo.findAll();
+    }
+
+    @GetMapping("/formations/{id}")
+    public ResponseEntity<Formation> getStagiaireById(@PathVariable int id) {
+        Formation formation = formationRepo.findById(id).orElseThrow(() -> new RuntimeException("No such Formation with id " + id));
+        return ResponseEntity.ok(formation);
+    }
+
+    @PostMapping("create-formation")
+    public Formation createFormation(@RequestBody Formation formation) {
+        return formationRepo.save(formation);
+    }
+
+
+}
--- api/src/main/java/com/api/repositories/FormationRepo.java
+++ api/src/main/java/com/api/repositories/FormationRepo.java
@@ -4,6 +4,9 @@ import com.api.entities.Formation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FormationRepo extends JpaRepository<Formation, Integer> {
+    Optional<Formation> findById(int id);
 }
