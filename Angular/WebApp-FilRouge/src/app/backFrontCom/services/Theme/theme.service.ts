b81d201bb563af0ab265f5f4cf111b0b32567f42
tekcay <pierreblc@hotmail.fr>
2023-01-19 12:33:16 +0100

    added method to retrieve all Theme or a specific Theme by its id from the database

--- Angular/WebApp-FilRouge/src/app/backFrontCom/services/Theme/theme.service.ts
+++ Angular/WebApp-FilRouge/src/app/backFrontCom/services/Theme/theme.service.ts
@@ -10,22 +10,16 @@ export class ThemeService {
 
   private baseUrl="http://localhost:8080/theme-controller";
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
 
-  get FormationList(): Observable<Theme[]>{
-    return this.httpClient.get<Theme[]>(this.baseUrl).pipe(
-      catchError(this.handleError)
-    );
+   }
+
+  public findAll(): Observable<Theme[]> {
+    return this.httpClient.get<Theme[]>(this.baseUrl+"/themes");
   }
 
- private handleError = (error: Response) => {
-    if (error.status === 400) {
-      return throwError("Bad input error");
-    }
-    if (error.status === 404) {
-      return throwError("Not found error");
-    }
-
-    return throwError("App error");
+  public findById(id: number): Observable<Theme> {
+    return this.httpClient.get<Theme>(this.baseUrl+"/themes/" + id);
   }
+
 }
