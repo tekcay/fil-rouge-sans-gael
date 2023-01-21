133d35abd9d8460e1dffece2a99c7b8b0337b6e9
tekcay <pierreblc@hotmail.fr>
2023-01-21 20:13:04 +0100

    added basic test class

---
+++ api/src/test/java/com/api/controllers/FormationControllerTest.java
@@ -0,0 +1,22 @@
+package com.api.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class FormationControllerTest {
+
+    @Test
+    void getAllFormation() {
+    }
+
+    @Test
+    void createFormationIsWorking() {
+
+    }
+}
