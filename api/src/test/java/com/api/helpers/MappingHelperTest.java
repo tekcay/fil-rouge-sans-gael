d3584afad50f9eace644d47ef8eedb52083e0ff6
tekcay <pierreblc@hotmail.fr>
2023-01-21 22:49:09 +0100

    added more tests for the MappingHelper

--- api/src/test/java/com/api/helpers/MappingHelperTest.java
+++ api/src/test/java/com/api/helpers/MappingHelperTest.java
@@ -1,9 +1,19 @@
 package com.api.helpers;
 
+import com.api.controllers.FormationController;
+import com.api.controllers.SThemeController;
 import com.api.controllers.ThemeController;
+import com.api.dto.FormationDTO;
+import com.api.dto.SThemeDTO;
 import com.api.dto.ThemeDTO;
+import com.api.entities.Formation;
+import com.api.entities.STheme;
+import com.api.entities.SsTheme;
 import com.api.entities.Theme;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,6 +33,48 @@ class MappingHelperTest {
 
         assertEquals(theme, convertedTheme);
         assertEquals(theme.getSThemes(), convertedTheme.getSThemes());
+    }
+
+    @Test
+    void shouldSThemeDTOUnMappedToSTheme() {
+
+        SThemeController sThemeController = new SThemeController();
+
+        SThemeDTO sThemeDTO = new SThemeDTO("Name");
+
+        STheme sTheme = new STheme();
+        sTheme.setName("Name");
+        sTheme.setId(1);
+
+        STheme convertedSTheme = sThemeController.unMapDTO(sThemeDTO, STheme.class);
+
+        assertEquals(sTheme, convertedSTheme);
+    }
+
+    @Test
+    void shouldFormationDTOUnMappedToFormation() {
+        FormationController formationController = new FormationController();
+        List<Integer> sThemesId = new ArrayList<>(){{add(1);}};
+        List<Integer> ssThemesId = new ArrayList<>(){{add(1);}};
+
+        STheme sTheme = new STheme();
+        sTheme.setId(1);
+        List<STheme> sThemeList = new ArrayList<>(){{add(sTheme);}};
+
+        SsTheme ssTheme = new SsTheme();
+        ssTheme.setId(1);
+        List<SsTheme> ssThemeList = new ArrayList<>(){{add(ssTheme);}};
+
+        FormationDTO formationDTO = new FormationDTO("Formation", "description", 1, sThemesId, ssThemesId);
+        Formation convertedFormation = formationController.unMapDTO(formationDTO, Formation.class);
+
+        Formation formation = new Formation();
+        formation.setName("Formation");
+        formation.setDescription("description");
+        formation.setSThemes(sThemeList);
+        formation.setSsThemes(ssThemeList);
+
+        assertEquals(formation, convertedFormation);
     }
 
 }
