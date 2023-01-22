acb66b4d78f93ff198886827e1dec75f9f4f719f
tekcay <pierreblc@hotmail.fr>
2023-01-22 13:52:24 +0100

    added tests for SThemeRepo and SsThemeRepo

--- api/src/main/java/com/api/repositories/SsThemeRepo.java
+++ api/src/main/java/com/api/repositories/SsThemeRepo.java
@@ -4,10 +4,12 @@ import com.api.entities.SsTheme;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SsThemeRepo extends JpaRepository<SsTheme, Integer> {
 
     Optional<SsTheme> findById(int id);
+    List<SsTheme> findAll();
 }
